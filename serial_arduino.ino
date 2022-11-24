00c6da75ae1e8be74c4c5eee260730b53f868925
TanCannon <98156505+TanCannon@users.noreply.github.com>
2022-11-24 14:00:30 +0530

    Add files via upload

---
+++ serial_arduino.ino
@@ -0,0 +1,43 @@
+
+ #include <LiquidCrystal_I2C.h>
+// //define I2C address......
+LiquidCrystal_I2C lcd(0x27,16,2);
+String incomingByte ;    
+int i;
+void setup() {
+  i = 0;
+   Serial.begin(9600);
+  //pinMode(LED_BUILTIN, OUTPUT);
+
+   // initialize the LCD
+   lcd.init();
+
+  // Turn on the blacklight and print a message.
+   lcd.backlight();
+  //lcd.print("Hello, world!");
+}
+
+void loop() {
+
+  if (Serial.available() > 0) {
+
+  incomingByte = Serial.readString();
+  lcd.clear();
+while(incomingByte[i] != NULL){
+  lcd.print(incomingByte[i]);
+  i = i + 1;
+}
+  i = 0;
+ // lcd.setCursor(0, 0);
+  //lcd.print(incomingByte);
+  delay(1000);
+  lcd.clear();
+  lcd.print("x");
+  delay(1000);
+
+  //Serial.write(incomingByte);
+
+   
+  }
+
+}
